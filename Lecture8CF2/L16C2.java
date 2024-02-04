b149fc0c92bdfa1d8c50757208387b9e7bdb4ad9
Harish <harishchowdary616@gmail.com>
2024-02-04 22:03:22 +0530

    version 1

---
+++ Lecture8CF2/L16C2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package net.codejava.cweek1;
+
+import java.util.Iterator;
+import java.util.Vector;
+
+/**
+ *
+ * @author Harish
+ */
+public class L16C2 {
+    public static void main(String[] args) {
+        Vector vec = new Vector();
+
+        vec.add("texas");
+        vec.add("zurich");
+        vec.add("queens");
+
+       // Iterator pointer = vec.Iterator();
+
+      //  while(pointer.hasNext()){
+        //    System.out.println(pointer.next());
+        }
+}
+
